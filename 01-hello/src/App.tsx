8125dd29c493c952382880fff053a39ddc546a1f
hhhuuunnnggg-design <nguyendinhhungtc2020@gmail.com>
2025-04-15 01:05:18 +0700

    first commit

---
+++ 01-hello/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HelloWord from "./component/HelloWord";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HelloWord />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-hello/src/component/HelloWord.tsx
@@ -0,0 +1,9 @@
+function HelloWord() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>This is a simple React component.</p>
+    </div>
+  );
+}
+export default HelloWord;
